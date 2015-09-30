6905ca6dc028b63599d815f70a2e36b2e7fc2036
Kelly Becker <kellywerks@gmail.com>
2015-09-30 09:40:29 -0700

    update details for google maps

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 
   actions: {
     showMap(rental) {
-      var container = this.$('.map-canvas')[0];
+      var container = this.$('.map-display')[0];
       var options = {
         center: this.get('map').center(rental.get('latitude'), rental.get('longitude')),
         zoom: 15
